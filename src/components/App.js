418dcffd8769e7ca4296b9623c5d5f9610d0959f
Deni Mazzei <dconkell@gmail.com>
2022-01-10 21:35:56 -0600

    added Nav.js to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Nav from "./Nav";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Adventure 2 Learning</h1>
+        <Nav />
       </div>
     );
   }
