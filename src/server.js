5322a8f4cac8c2932811025febeb469855ce8e42
Deni Mazzei <dconkell@gmail.com>
2022-01-10 19:49:14 -0600

    added apollo server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
-const path = require("path");
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const cors = require("cors");
+
+const path = require("path");
 const app = express();
 
 const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
+const server = new ApolloServer({});
+
+server.applyMiddleware({ app });
+
+app.use(cors());
 
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
