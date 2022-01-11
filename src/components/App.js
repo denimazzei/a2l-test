db5fb915c2a7f3147b412deb6bd2ec20d6953563
Deni Mazzei <dconkell@gmail.com>
2022-01-10 21:15:39 -0600

    moved app.js file to components folder

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Adventure 2 Learning</h1>
+      </div>
+    );
+  }
+}
+export default App;
