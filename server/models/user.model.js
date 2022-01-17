c047deccea060aff23adfd45e39e48cfb5ab199f
Deni Mazzei <dconkell@gmail.com>
2022-01-16 19:53:27 -0600

    define and initialize mongoose

---
+++ server/models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role",
+      },
+    ],
+  })
+);
+
+module.exports = User;
