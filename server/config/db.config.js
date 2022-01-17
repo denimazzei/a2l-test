4cb97d9deb88dc77c8572509476b2a0b29088864
Deni Mazzei <dconkell@gmail.com>
2022-01-16 21:59:51 -0600

    added utils with auth.js

--- server/config/db.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  PORT: 27017,
-  DB: "a2l-app",
-};
--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,3 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+const Profile = require("./Profile");
 
-const db = {};
-
-db.mongoose = mongoose;
-
-db.user = require("./user.model");
-db.role = require("./role.model");
-
-db.ROLES = ["user", "admin", "moderator"];
-
-module.exports = db;
+module.exports = { Profile };
